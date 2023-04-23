727a526c1ba8427ce34baa363296cfad12447d05
Dana299 <gorevadana@gmail.com>
2023-04-23 17:38:20 +0300

    feat: add endpoint for reading product state records

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -1,11 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from stats.views import ProductTrackingViewSet
 
-router = SimpleRouter()
-router.register(r'trackings', ProductTrackingViewSet)
+from stats.views import ProductHistoryViewSet, ProductTrackingViewSet
+
+trackings_router = SimpleRouter()
+trackings_router.register(r'trackings', ProductTrackingViewSet, basename='trackings')
+
+history_router = SimpleRouter()
+history_router.register(r'product-states', ProductHistoryViewSet, basename='product-states')
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(trackings_router.urls)),
+    path('', include(history_router.urls)),
 ]
